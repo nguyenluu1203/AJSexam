d1fe9e4a690dc5e94b912c482ecdb8176410dc32
nguyenluu1203 <115715762+nguyenluu1203@users.noreply.github.com>
2022-12-26 11:14:04 +0700

    update

--- src/app/pages/slide-bar/slide-bar.component.ts
+++ src/app/pages/slide-bar/slide-bar.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-slide-bar',
   templateUrl: './slide-bar.component.html',
-  styleUrls: ['./slide-bar.component.css']
 })
 export class SlideBarComponent {
 
